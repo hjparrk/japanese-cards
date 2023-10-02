4f7439573fe8695df2f45c0376c3e58da1358227
Hyojoon Park <hyojoon2372552@naver.com>
2023-10-03 01:44:36 +1100

    add current card number

--- src/components/CardContent.jsx
+++ src/components/CardContent.jsx
@@ -1,3 +1,4 @@
+import { Fragment } from "react";
 import { useState } from "react";
 import { useEffect } from "react";
 
@@ -11,21 +12,26 @@ const CardContent = (props) => {
   }, [num]);
 
   return (
-    <div className="flex flex-col gap-5 w-32 h-[20vh] items-center justify-start">
-      <h1 className="text-8xl p-3">{word.japanese}</h1>
-      {!show && (
-        <button
-          className="text-base"
-          onClick={(e) => {
-            e.preventDefault();
-            setShow(true);
-          }}
-        >
-          Show Answer
-        </button>
-      )}
-      {show && <h1 className="text-3xl">{word.romanised}</h1>}
-    </div>
+    <Fragment>
+      <div className="flex flex-col gap-5 w-32 h-[20vh] items-center pt-4">
+        <div>
+          <h1 className="text-8xl p-3">{word.japanese}</h1>
+          <h1 className="text-sm">{`[ ${num + 1} / 46 ]`}</h1>
+        </div>
+        {!show && (
+          <button
+            className="text-lg"
+            onClick={(e) => {
+              e.preventDefault();
+              setShow(true);
+            }}
+          >
+            Show Answer
+          </button>
+        )}
+        {show && <h1 className="text-3xl">{word.romanised}</h1>}
+      </div>
+    </Fragment>
   );
 };
 
